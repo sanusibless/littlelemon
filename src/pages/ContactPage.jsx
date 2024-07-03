c17f79adef9a77efeff25c483f89ad75f7fe239c
Blessing Sanusi <holuwoleh@gmail.com>
2024-07-03 20:51:18 +0100

    Added more pages

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,8 +1,9 @@
-import React from "react"
+import React, { useState } from "react"
 import Header from "../component/Header"
 import Footer from "../component/Footer"
 import contactimg from "../icons_assets/img/contact-us1.jpg"
 import ResponseComponent from "../component/response/ResponseComponent"
+
 
 export default function ContactPage() {
 
@@ -15,6 +16,7 @@ export default function ContactPage() {
       const [error, setError] = useState({});
       const [response, setResponse] = useState({});
       const [displayResponse, setDiplayResponse] = useState(false);
+      const [loading, setLoading] = useState(null)
    
       function clearError(e) {
       const { name } = e.target;
@@ -23,31 +25,37 @@ export default function ContactPage() {
         return prevErr
       });
      }
-  
+
+        let prevent = false;
+        for(const key of Object.keys(data)) {
+            if(data[key] == '' || data[key].length <=1 ) prevent = true
+        }
+
      function handleChange(e) {
       const { name, value} = e.target;
-        
         if (value == '' || value.length <= 1) {
           setError(prevErr => ({
             ...prevErr,
             [name] :  name + ' is required'
           }))
-        }
+    } else {
+        setError( prevErr => {
+            delete prevErr[name];
+            return prevErr
+          });
+    }
 
         setData( prevData => ({
-          ...prevData,
-          [e.target.name] : e.target.value
+            ...prevData,
+            [name] : value
         }))
+
       }
   
      function validateData(data) {
       let valid = true;
-  
       for(const key of Object.keys(data) ) {
-        if(key == 'specialRequest') {
-          continue
-        }
-        if(data[key] == 0 || (data[key] == '' || data[key].length <= 0) ) {
+        if(data[key] == '' || data[key].length <= 1) {
           setError(prevErr => ({
             ...prevErr,
             [key] : key + ' is required'
@@ -61,31 +69,30 @@ export default function ContactPage() {
       return valid;
      }
   
+        
       function successResponse() 
       {
+        setLoading(true);
         setDiplayResponse(true);
         setResponse({
           status: 'success',
           message: 'Thanks for leaving a message. We will get back to your shortly'
         })
         setData({
-          date: '',
-          time: '',
-          numberOfGuests: 0,
-          specialRequest: '',
-          occassion: '',
+            name: '',
+            phone: '',
+            email: '',
+            message: ''
         });
         setError({});
+        setTimeout(() => {
+            setDiplayResponse(false)
+            setLoading()
+        }, 10000)
+
+        setLoading(false)
+
       }
-  
-    function errorResponse() {
-      setDiplayResponse(true);
-      setResponse({
-        status : 'error',
-        message: 'Please check inputs and try again'
-      })
-      // setDiplayResponse(false);
-    }
     
    const handleFormSubmission = (e) => {
       e.preventDefault();
@@ -113,24 +120,28 @@ export default function ContactPage() {
                     <h3>Feel Free to message us</h3>
                     <div>
                         <label className="contact-label" htmlFor="name">Name</label>
-                        <input onFocus={clearError} onChange={handleChange} className='contact-input' type="text" name="name" />
+                        <input value={data.name} onChange={handleChange} className='contact-input' type="text" name="name" />
+                        { error && error.name != '' && <small className='error'>{error?.name}</small> }
                     </div>
                     <div>
                         <label className="contact-label">Email</label>
-                        <input onFocus={clearError} onChange={handleChange} className='contact-input' type="email" name="email" />
+                        <input value={data.email} onChange={handleChange} className='contact-input' type="email" name="email" />
+                        { error && error.email != '' && <small className='error'>{error?.email}</small> }
                     </div>
                     <div>
                         <label className="contact-label" >Phone</label>
-                        <input onFocus={clearError} onChange={handleChange} className='contact-input' name="phone" type="tel" pattern="0[7-9][0-1][0-9]{8}" />
+                        <input value={data.phone} onChange={handleChange} className='contact-input' name="phone" type="tel" pattern="0[7-9][0-1][0-9]{8}" />
+                        { error && error.phone != '' && <small className='error'>{error?.phone}</small> }
                     </div>
                     <div>
                         <label className="contact-label">Leave a message</label>
-                        <textarea onFocus={clearError} name="message">
+                        <textarea  value={data.message} onChange={handleChange} name="message">
 
                         </textarea>
+                        { error && error.message != '' && <small className='error'>{error?.message}</small> }
                     </div>
                     <div className="contact-btn-div">
-                        <button className="contact-btn">
+                        <button type="submit" className="contact-btn">
                             Send
                         </button>
                     </div>
